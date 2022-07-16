06c32440bc44c412f5922e274472245ac617adf5
gabriel lucas <gabriel69.lucas@gmail.com>
2022-07-16 15:34:58 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>oi</h1>
-  )  
+  return (
+    <>
+      <h1>oi</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
